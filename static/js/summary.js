ef66166dcf200dacbe889f5dcd00f4e185e68967
Zubair Shaikh <zsescape@gmail.com>
2022-12-24 18:35:13 -0500

    Update index.html - Add Add app.py - Add summary.js

---
+++ static/js/summary.js
@@ -0,0 +1,99 @@
+const url = "/api/summary.json"
+
+d3.select("#aggregate-function").on("change", handleChange)
+
+const aggregateFunctionLabel = {
+    mean: "Average",
+    min: "Minimum",
+    max: "Maximum",
+    var: "Variance",
+    std: "Standard Deviation"
+}
+
+function toTitleCase(string) {
+    return `${string[0].toUpperCase()}${string.slice(1).toLowerCase()}`
+}
+
+function plotCharts(aggregateFunction) {
+    d3.json(url).then(data => {
+        const metrics = [
+            "acousticness",
+            "danceability",
+            "energy",
+            "instrumentalness",
+            "liveness",
+            "speechiness",
+            "valence",
+        ]
+
+        const summaryLinePlotLayout = {
+            title: `${aggregateFunctionLabel[aggregateFunction]} of Acousticness, Danceability, Energy, Instrumentalness, Liveness, Speechiness, Valence over the years`
+        }
+
+        const traces = metrics.map(metric => ({
+            x: data.map(_ => _["year"]),
+            y: data.map(_ => _[`${metric}_${aggregateFunction}`]),
+            type: "scatter",
+            name: toTitleCase(metric)
+        }))
+
+        Plotly.newPlot("summary-line-plot", traces, summaryLinePlotLayout, {responsive: true})
+
+        const loudnessTempoLinePlotTrace = [{
+            x: data.map(_ => _["year"]),
+            y: data.map(_ => _[`loudness_${aggregateFunction}`]),
+            type: "scatter",
+            name: "Loudness"
+        }, {
+            x: data.map(_ => _["year"]),
+            y: data.map(_ => _[`tempo_${aggregateFunction}`]),
+            type: "scatter",
+            name: "Tempo"
+        }]
+
+        const loudnessTempoLinePlotLayout = {
+            title: `${aggregateFunctionLabel[aggregateFunction]} of Loudness and Tempo over the years`
+        }
+
+        Plotly.newPlot("loudness-tempo-line-plot", loudnessTempoLinePlotTrace, loudnessTempoLinePlotLayout, {responsive: true})
+
+        const explicitLinePlotTrace = [{
+            x: data.map(_ => _["year"]),
+            y: data.map(_ => _["explicit_ratio"]),
+            type: "scatter",
+            name: "Explicity"
+        }]
+
+        const explicitLinePlotLayout = {
+            title: "Explicity over the years"
+        }
+
+        Plotly.newPlot("explicit-line-plot", explicitLinePlotTrace, explicitLinePlotLayout, {responsive: true})
+
+        const totalLinePlotTrace = [{
+            x: data.map(_ => _["year"]),
+            y: data.map(_ => _["total"]),
+            type: "scatter",
+            name: "Total Songs"
+        }]
+
+        const totalLinePlotLayout = {
+            title: "Total songs over the years"
+        }
+
+        Plotly.newPlot("total-line-plot", totalLinePlotTrace, totalLinePlotLayout, {responsive: true})
+
+
+    })
+
+}
+
+function handleChange(event) {
+    const aggregateFunction = event.target.value
+    console.log(aggregateFunction)
+    plotCharts(aggregateFunction)
+}
+
+(() => {
+    plotCharts("mean")
+})()
