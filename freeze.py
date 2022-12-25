9c00045c74c72916c46e66c5954ffed043d6e339
Zubair Shaikh <zsescape@gmail.com>
2022-12-24 19:55:59 -0500

    Update freeze.py - Add 'FREEZER_BASE_URL'

--- freeze.py
+++ freeze.py
@@ -1,7 +1,7 @@
 from flask_frozen import Freezer
 from app import app
 
-freezer = Freezer(app)
-
 if __name__ == '__main__':
+    app.config["FREEZER_BASE_URL"] = "https://compmonk.github.io/SongStats/"
+    freezer = Freezer(app)
     freezer.freeze()
