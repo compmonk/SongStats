f2ffa7ba25d86bc6a3788904e644ca7d864591ff
Zubair Shaikh <zsescape@gmail.com>
2022-12-26 04:38:09 -0500

    Add Dark Mode - Move JS code from base.html to base.js - Add class="plot" for all plots - Add font, paper_bgcolor and plot_bgcolor for plotly plots - Add dark theme for Select 2

---
+++ static/js/base.js
@@ -0,0 +1,80 @@
+const lightModeButton = d3.select("#light-mode")
+const darkModeButton = d3.select("#dark-mode")
+const root = document.querySelector(":root")
+console.log(root)
+const html = d3.select("html")
+
+const screenMode = localStorage.getItem("theme") || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light")
+const plots = d3.selectAll(".plot")
+
+lightModeButton.on("click.tr", transitionToLightMode);
+lightModeButton.on("click.up", updatePlotly);
+darkModeButton.on("click.tr", transitionToDarkMode);
+darkModeButton.on("click.up", updatePlotly);
+
+const aggregateFunctionLabel = {
+    mean: "Average",
+    min: "Minimum",
+    max: "Maximum",
+    var: "Variance",
+    std: "Standard Deviation"
+};
+
+const darkModeColors = {
+    "--border-color": "#495057",
+    "--background-color": "#212529",
+    "--font-color": "#FFFFFF"
+}
+
+const lightModeColors = {
+    "--border-color": "#dee2e6",
+    "--background-color": "#FFFFFF",
+    "--font-color": "#000000",
+}
+
+let backgroundColor = lightModeColors["--background-color"];
+let fontColor = lightModeColors["--font-color"];
+
+function toTitleCase(string) {
+    return `${string[0].toUpperCase()}${string.slice(1).toLowerCase()}`
+}
+
+function transitionToLightMode() {
+    html.attr("data-bs-theme", null)
+    lightModeButton.classed("hide", true)
+    darkModeButton.classed("hide", false)
+    Object.entries(lightModeColors).map(([key, value]) => root.style.setProperty(key, value))
+    backgroundColor = lightModeColors["--background-color"];
+    fontColor = lightModeColors["--font-color"];
+    localStorage.setItem("theme", "light")
+}
+
+function transitionToDarkMode() {
+    html.attr("data-bs-theme", "dark")
+    lightModeButton.classed("hide", false)
+    darkModeButton.classed("hide", true)
+    Object.entries(darkModeColors).map(([key, value]) => root.style.setProperty(key, value))
+    backgroundColor = darkModeColors["--background-color"];
+    fontColor = darkModeColors["--font-color"];
+    localStorage.setItem("theme", "dark")
+}
+
+function updatePlotly() {
+    plots.select(function () {
+        Plotly.relayout(this, {
+            font: {
+                color: fontColor,
+            },
+            paper_bgcolor: backgroundColor,
+            plot_bgcolor: backgroundColor
+        })
+    })
+}
+
+(() => {
+    if (screenMode === "light") {
+        transitionToLightMode();
+    } else {
+        transitionToDarkMode()
+    }
+})()
