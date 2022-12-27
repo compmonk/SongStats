2064640cf9e38d9adcf462166644430d1b1b8ba8
Zubair Shaikh <zsescape@gmail.com>
2022-12-27 15:26:09 -0500

    Update summary.js and comparison.js - Add "duration" to line plots - Update xaxis label for "Musical Key distribution over the years" bar chart - Update xaxis label for "Time Signature distribution over the years" bar chart

--- static/js/summary.js
+++ static/js/summary.js
@@ -44,6 +44,11 @@ function plotCharts(aggregateFunction) {
             y: data.map(_ => _[`tempo_${aggregateFunction}`]),
             type: "scatter",
             name: "Tempo"
+        }, {
+            x: data.map(_ => _["year"]),
+            y: data.map(_ => _[`duration_${aggregateFunction}`]),
+            type: "scatter",
+            name: "Duration (s)"
         }]
 
         const loudnessTempoLinePlotLayout = {
@@ -115,7 +120,6 @@ function plotCharts(aggregateFunction) {
 
 function handleChange(event) {
     const aggregateFunction = event.target.value
-    console.log(aggregateFunction)
     plotCharts(aggregateFunction)
 }
 
